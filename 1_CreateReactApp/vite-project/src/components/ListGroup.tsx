481f25982d53764e0aea7b21e8031efff8ba7eb5
DaryllCulas <culasdaryll99@gmail.com>
2023-09-21 03:08:53 +0800

    add components

---
+++ 1_CreateReactApp/vite-project/src/components/ListGroup.tsx
@@ -0,0 +1,39 @@
+import { useState } from "react";
+
+function ListGroup() {
+  let items = [
+    "New York",
+    "Davao City",
+    "Negros Oriental",
+    "Brooklyn",
+    "San Francisco",
+  ];
+
+  //Hook
+  const [selectedIndex, setSelectedIndex] = useState(-1);
+  return (
+    <>
+      <h1>List</h1>
+      {items.length === 0 && <p>No item found</p>}
+      <ul className="list-group">
+        {items.map((item, index) => (
+          <li
+            className={
+              selectedIndex === index
+                ? "list-group-item active"
+                : "list-group-item"
+            }
+            key={item}
+            onClick={() => {
+              setSelectedIndex(index);
+            }}
+          >
+            {item}
+          </li>
+        ))}
+      </ul>
+    </>
+  );
+}
+
+export default ListGroup;
