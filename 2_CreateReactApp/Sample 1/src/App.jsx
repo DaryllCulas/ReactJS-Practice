3e1dce7dd88ed2874aebd704eedcf1853ec7edca
DaryllCulas <culasdaryll99@gmail.com>
2023-09-24 22:12:11 +0800

    Add Second react-app for practice purpose

---
+++ 2_CreateReactApp/Sample 1/src/App.jsx
@@ -0,0 +1,10 @@
+import Hello from './Hello';
+
+
+// Displaying component here from imported module ('Hello.jsx');
+function App() {
+
+  return <Hello />;
+}
+
+export default App
---
+++ 2_CreateReactApp/Sample 1/src/Hello.jsx
@@ -0,0 +1,10 @@
+
+// First creation of component here
+function Hello() {
+  return <h1>Hello World</h1>
+}
+
+
+
+// Export here 
+export default Hello;
