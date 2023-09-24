13b3761485c9d37a02ce27fc3487c4f9a5988e13
DaryllCulas <culasdaryll99@gmail.com>
2023-09-24 23:30:24 +0800

    Modified destructuring props

--- 2_CreateReactApp/Sample 1/src/App.jsx
+++ 2_CreateReactApp/Sample 1/src/App.jsx
@@ -2,7 +2,15 @@ import Hello from "./Hello";
 
 // Displaying component here from imported module ('Hello.jsx');
 function App() {
-  return <Hello name="Daryll" />;
+  return (
+    <div>
+      <h1>
+        <Hello name="Daryll" age="24" />
+        <Hello name="Luffy" age="20" />
+        <Hello name="Zoro" age="22" />
+      </h1>
+    </div>
+  );
 }
 
 export default App;
--- 2_CreateReactApp/Sample 1/src/Hello.jsx
+++ 2_CreateReactApp/Sample 1/src/Hello.jsx
@@ -1,9 +1,9 @@
-// First creation of component here with props
-function Hello({ name }) {
+// First creation of component here with destructured props
+
+function Hello({ name, age }) {
   return (
     <h1>
-      Hello World!
-      {name}
+      Hello World!, Hey there {name}, I'm {age} years old
     </h1>
   );
 }
