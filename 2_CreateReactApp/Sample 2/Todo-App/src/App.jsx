a1f807fe99da63dc91473a7173736043feea06b5
DaryllCulas <culasdaryll99@gmail.com>
2023-10-02 03:27:16 +0800

    Inline stylesheet using CSS modules

--- 2_CreateReactApp/Sample 2/Todo-App/src/App.jsx
+++ 2_CreateReactApp/Sample 2/Todo-App/src/App.jsx
@@ -1,8 +1,11 @@
+import Header from "./components/Header";
 import { TodoApp } from "./components/TodoApp";
+import "./components/css/App.css";
 
 function App() {
   return (
     <div>
+      <Header />
       <TodoApp />
     </div>
   );
---
+++ 2_CreateReactApp/Sample 2/Todo-App/src/components/Header.jsx
@@ -0,0 +1,7 @@
+import styles from "./css/Header.module.css";
+
+const Header = () => {
+  return <div className={styles.header}>MY TODO-LISTS</div>;
+};
+
+export default Header;
