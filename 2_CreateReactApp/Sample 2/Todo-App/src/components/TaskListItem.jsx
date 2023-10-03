9b07e6de83468573ed3a909ca28d98db35487849
DaryllCulas <culasdaryll99@gmail.com>
2023-10-04 00:35:49 +0800

    Adding a delete button

--- 2_CreateReactApp/Sample 2/Todo-App/src/components/TaskListItem.jsx
+++ 2_CreateReactApp/Sample 2/Todo-App/src/components/TaskListItem.jsx
@@ -2,7 +2,13 @@ import styles from "./css/TaskListItem.module.css";
 const TaskListItem = ({ item }) => {
   return (
     <div className={styles.item}>
-      <div className={styles.itemName}>{item}</div>
+      <div className={styles.itemName}>
+        {item}
+        <span>
+          <button className={styles.deleteButton}>X</button>
+        </span>
+      </div>
+
       <hr className={styles.line} />
     </div>
   );
